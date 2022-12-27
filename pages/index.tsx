af0af7615c5a78e1387b5e5f9d8e89a2e4733a96
Muzammilhasnain <mk36904276@gmail.com>
2022-12-27 22:55:15 +0500

    d

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,8 +26,8 @@ export default function Home() {
                       
                       <div className="offcanvas offcanvas-start"  id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                         <div className="offcanvas-header">
-                          <h5 className="offcanvas-title offbod title" id="offcanvasExampleLabel">Rush</h5>
-                          <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                          <h5 className="offcanvas-title offbod title " id="offcanvasExampleLabel">Rush</h5>
+                          <button type="button" className="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
                         <div className="offcanvas-body offbod">
                           <div>
